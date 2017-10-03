56d73e48ba7e7bbf3e565c755d4a52467174d874
aplicacionesweb1informatica <aplicacionesweb1informatica.gmail.com>
2017-10-03 12:57:08 -0500

    estos archivos fueron agregados desde el equipo

---
+++ administrador/formulario_administrador.php
@@ -0,0 +1,9 @@
+<?php
+
+/* 
+ * Este formulario fue creado desde el equipo
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
